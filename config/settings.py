90c596cd65c63f691308e066fbfcf9b8398f2966
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-05 10:55:15 +0000

    💄 add icons

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
