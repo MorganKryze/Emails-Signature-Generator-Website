2ebd7bb2ba64f533702d16ee024b20676df29976
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-08 13:15:11 +0000

    🌟 working icons socials

--- generator/tools.py
+++ generator/tools.py
@@ -11,5 +11,32 @@ def generate_html(data: dict):
 
     template = env.get_template(HTML_TEMPLATE)
     data_conf_dict = dict(data)
+    
+    social_media_html = ""
+    socials = data_conf_dict["socials"]
+
+    social_media_mapping = {
+        "web": "website",
+        "github": "github",
+        "instagram": "instagram",
+        "linkedin": "linkedin",
+        "slack": "slack",
+        "youtube": "youtube",
+        "twitter": "twitter",
+        "facebook": "facebook",
+    }
+
+    for key, value in socials.items():
+        if key.endswith("_link"):
+            social_media = social_media_mapping[key.split("_")[0]]
+            if value != "None":
+                social_media_html += (
+                    f'\n\t\t\t\t<a href="{value}"><img'
+                    ' src="https://raw.githubusercontent.com/MorganKryze/Emails-Signature-Generator-Website/main/assets/img/icons/'
+                    f'{social_media}.svg" alt="{social_media}_icon"></a>'
+                )
+
+    data_conf_dict["social_media_html"] = social_media_html
+
 
     return template.render(data_conf_dict)
